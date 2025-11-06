f72353a1a0822da3ee5fb2420b110ab609cfaa72
PARDEEP 111 <pardeepsingh091209@gmail.com>
2025-11-06 18:16:15 +0530

    learnig middlware starting

---
+++ middlwares/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const porat = 3000
+const fs = require('fs')
+app.use('/', (req,res,next)=>{
+    console.log("hie");
+    next();
+})
+
+app.use('/', (req,res,next)=>{
+    fs.writeFileSync("logs.txt", `${Date.now()} is a ${req.method}`)
+    console.log(`${Date.now()} is a ${req.method}`);
+    next();
+})
+
+app.get('/', (req,res)=>{
+    res.send('hello world')
+})
+app.get('/about', (req,res)=>{
+    res.send('hello about')
+})
+
+
+app.listen(porat, ()=>{
+console.log(`here is the port ${porat}`);
+
+})
+console.log("Running file:", __filename)
